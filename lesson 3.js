abcb5fd87b5ab16e977219b04dad12125f4c9a3b
olnovikov <oleg-novikov26@yandex.ru>
2020-10-14 15:12:12 +0300

    taskSigned-off-by: olnovikov <oleg-novikov26@yandex.ru>

---
+++ lesson 3.js
@@ -0,0 +1,47 @@
+//1
+let n = 2;
+let arr = [];
+while (n < 100) {
+    for (let index = 2; index <= n; index++) {
+        if (n % index == 0 && index !== n) {
+
+            break
+
+        }
+        else {
+            if (index == n) {
+                arr.push(n);
+
+            }
+
+        }
+
+    }
+    n++;
+}
+
+
+
+alert(arr);
+
+//2)
+
+
+function countBasketPrice(arr) {
+    for (let index = 0; index < array.length; index++) {
+        return sum += arr[i];
+
+    }
+
+}
+
+
+//3)
+for (let index = 0; index < 10; alert(index++)) { }
+
+//4)
+const array = []
+for (let index = 0; index < 20; index++) {
+    array.push('x')
+    console.log(array.join(''))
+}
